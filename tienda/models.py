4be2dc5aa0956cebb922e5142824bd1d7268c28b
NaturalKiIllers <matiasmeloguillaume@gmail.com>
2020-10-27 19:33:42 -0300

    xd

--- tienda/models.py
+++ tienda/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Figura(models.Model):
     id_figura        = models.AutoField(db_column='id_figura', primary_key=True)
+    codigof           = models.CharField(max_length=10, unique=True,null=True)
     nombre           = models.CharField(max_length=20, blank=True, null=True)
     anime            = models.CharField(max_length=20, blank=True, null=True)
     descripcion      = models.CharField(max_length=50, blank=True, null=True)
@@ -29,3 +30,11 @@ class Artbook(models.Model):
     nombre_desc      = models.CharField(max_length=50, blank=True, null=True)
     descripcion      = models.CharField(max_length=100, blank=True, null=True)
     fotoArtbook      = models.ImageField(upload_to='fotos', blank=True, null=True)
+
+class Contacto(models.Model):
+    id_contacto       = models.AutoField(db_column='id_contacto', primary_key=True)
+    nombres           = models.CharField(max_length=50, blank=True, null=True)
+    correo            = models.CharField(max_length=50, blank=True, null=True)
+    asunto            = models.CharField(max_length=10, blank=True, null=True)
+    mensaje           = models.CharField(max_length=120, blank=True, null=True)
+  
